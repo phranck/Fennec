dd2f33b8c30208e14630c2a0db617e286b96e6c8
Frank Gregor <phranck@mac.com>
2024-07-16 23:59:19 +0200

    adds computed property `version`

--- Sources/Fennec/Fennec.swift
+++ Sources/Fennec/Fennec.swift
@@ -16,4 +16,9 @@ public extension Fennec {
     var isInitialized: Bool {
         lv_is_initialized()
     }
+
+    var version: String {
+        guard let cVersion = lv_version_info() else { return "n/a" }
+        return String(cString: cVersion)
+    }
 }
