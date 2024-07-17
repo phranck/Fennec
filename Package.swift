936eef800d212afa8ef87b1509645dfdce9879d7
Frank Gregor <phranck@woodbytes.me>
2024-07-17 18:31:34 +0200

    adds some compiler flags in Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,19 @@ let package = Package(
             cSettings: [
                 .headerSearchPath("."),
                 .define("LV_CONF_INCLUDE_SIMPLE"),
-                .define("THREAD_SAFE")
+                .define("THREAD_SAFE"),
+                .define("LV_CONF_MICROCONTROLLER"),
+                .define("LV_USE_SMALL_DISPLAY", to: "0"),
+                .define("LV_USE_TINY_TTF", to: "0"),
+                .define("LV_USE_GPU", to: "1"),
+                .define("LV_USE_PERF_MONITOR", to: "0"),
+                .define("LV_USE_LOG", to: "1"),
+                .define("LV_MEMCPY_MEMSET_STD", to: "1"),
+                .define("LV_USE_ASSERT_NULL", to: "0"),
+                .define("LV_USE_ASSERT_MALLOC", to: "0"),
+                .define("LV_TICK_CUSTOM", to: "1"),
+                .define("LV_COLOR_DEPTH", to: "16"),
+                .define("LV_COLOR_16_SWAP", to: "0"),
             ]
         )
     ]
